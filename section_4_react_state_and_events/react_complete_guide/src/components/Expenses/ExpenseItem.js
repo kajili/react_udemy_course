54acce844faffb7de06902a006d98cff7f2762e7
Kevin Ajili <kajili@ucsc.edu>
2021-04-20 14:15:14 -0700

    Working with user input

--- section_4_react_state_and_events/react_complete_guide/src/components/Expenses/ExpenseItem.js
+++ section_4_react_state_and_events/react_complete_guide/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
   const [title, setTitle] = useState(props.title);
-    
+
   const clickHandler = () => {
     setTitle("Updated!");
     console.log(title);
-  }
-  
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
