932a5304f3050245282dd5ae9b6b2429976a7c8d
Kevin Ajili <kajili@ucsc.edu>
2021-04-26 14:43:38 -0700

    fixed typos

--- section_23_react_summary/react-course-summary-part-2/src/components/meetups/NewMeetupForm.js
+++ section_23_react_summary/react-course-summary-part-2/src/components/meetups/NewMeetupForm.js
@@ -15,16 +15,15 @@ function NewMeetupForm() {
           <input type="url" required id="image" />
         </div>
         <div className={classes.control}>
-          <label htmlFor="address">Meetup Address</label>
+          <label htmlFor="address">Address</label>
           <input type="text" required id="address" />
         </div>
         <div className={classes.control}>
-          <label htmlFor="description">Meetup Image</label>
-          <textarea id="description" required rows='5'></textarea>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" required rows="5"></textarea>
         </div>
         <div className={classes.actions}>
-            <button type="button">Add Meetup</button>
-
+          <button type="button">Add Meetup</button>
         </div>
       </form>
     </Card>
