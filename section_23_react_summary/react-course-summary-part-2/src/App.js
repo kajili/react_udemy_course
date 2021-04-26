000c5c354129f9fd8984f893d672331d6f44c98c
Kevin Ajili <kajili@ucsc.edu>
2021-04-26 13:23:55 -0700

    Added basic navigation links

--- section_23_react_summary/react-course-summary-part-2/src/App.js
+++ section_23_react_summary/react-course-summary-part-2/src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   // localhost:3000/
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
