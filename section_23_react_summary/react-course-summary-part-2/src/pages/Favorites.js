97e993f028d0af643b0a54729cf69f61fc070a6c
Kevin Ajili <kajili@ucsc.edu>
2021-04-27 16:03:56 -0700

    update

--- section_23_react_summary/react-course-summary-part-2/src/pages/Favorites.js
+++ section_23_react_summary/react-course-summary-part-2/src/pages/Favorites.js
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 
 import FavoritesContext from "../store/favorites-context";
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
 function FavoritesPage() {
   const favoritesCtx = useContext(FavoritesContext);
 
   let content;
 
-  if(favoritesCtx.totalFavorites === 0) {
-    content = <p>You got no favorites yet. Start adding some?</p>
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
   } else {
-    content = <MeetupList meetups={favoritesCtx.favorites} />
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
   }
   return (
     <section>
